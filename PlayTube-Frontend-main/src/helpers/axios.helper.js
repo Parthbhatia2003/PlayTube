238e026c6e6a21383e7a8971d0394ce92c289feb
Parthbhatia2003 <139068349+Parthbhatia2003@users.noreply.github.com>
2024-07-03 16:02:37 +0530

    Update axios.helper.js

--- PlayTube-Frontend-main/src/helpers/axios.helper.js
+++ PlayTube-Frontend-main/src/helpers/axios.helper.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // import { loadProgressBar } from "axios-progress-bar";
 // import "axios-progress-bar/dist/nprogress.css";
 
-const baseURL = "http://localhost:8000/api/v1";
+const baseURL = "https://play-tube-api-indol.vercel.app/api/v1";
 
 export const axiosInstance = axios.create({
   baseURL,
